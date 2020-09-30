a5896f28919d64e123230cfe24b919f8124fa408
Jeremy Yip <jeremy.yip@automattic.com>
2020-09-30 09:05:22 -0700

    Add slide down animation

--- packages/edit-site/src/components/header/document-actions/index.js
+++ packages/edit-site/src/components/header/document-actions/index.js
@@ -102,7 +102,9 @@ export default function DocumentActions( { documentTitle } ) {
 	return (
 		<div
 			className={ classnames( 'edit-site-document-actions', {
+				// Handles slide up and slide down animations
 				'has-secondary-label': !! label,
+				'has-no-secondary-label': ! label,
 			} ) }
 		>
 			{ documentTitle ? (
@@ -124,7 +126,6 @@ export default function DocumentActions( { documentTitle } ) {
 							'edit-site-document-actions__label',
 							'edit-site-document-actions__secondary-item',
 							{
-								// 'is-active': isActive,
 								'is-secondary-title-active': isActive,
 							}
 						) }
