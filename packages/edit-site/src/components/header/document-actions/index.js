7b72dc4dd42f8c51d2e07e7b42548c285f00ad6a
Jeremy Yip <jeremy.yip@automattic.com>
2020-09-30 12:51:08 -0700

    Implement transition instead of animation

--- packages/edit-site/src/components/header/document-actions/index.js
+++ packages/edit-site/src/components/header/document-actions/index.js
@@ -102,9 +102,7 @@ export default function DocumentActions( { documentTitle } ) {
 	return (
 		<div
 			className={ classnames( 'edit-site-document-actions', {
-				// Handles slide up and slide down animations
 				'has-secondary-label': !! label,
-				'has-no-secondary-label': ! label,
 			} ) }
 		>
 			{ documentTitle ? (
